c0638d6a130b978550b774e2fe1db20dfccb79cc
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2016-06-30 22:03:32 +0930

    Use additional plugins

--- app/index.js
+++ app/index.js
@@ -3,5 +3,10 @@ import config from './config'
 
 const argo = new Argo()
 
+// Core
 argo.use(require('./modules/argo-core').default(argo, config))
 argo.use(require('./modules/argo-memory-cache').default(argo))
+
+// Plugins
+argo.use(require('./modules/argo-convert').default(argo, config))
+argo.use(require('./modules/argo-yahoocurrency').default(argo, config))
