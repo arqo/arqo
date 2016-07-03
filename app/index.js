9822e919309fa740fa659b34badc0dfae092abfe
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2016-07-03 22:12:08 +0930

    Update index file with simple example

--- app/index.js
+++ app/index.js
@@ -5,13 +5,14 @@ const argo = new Argo()
 
 // Core
 argo.use(require('./modules/argo-core').default(argo, config))
-//argo.use(require('./modules/argo-memory-cache').default(argo))
+argo.use(require('./modules/argo-memory-cache').default(argo))
 
 // Plugins
-argo.use(require('./modules/argo-convert').default(argo, config))
-//argo.use(require('./modules/argo-yahoocurrency').default(argo, config))
+argo.use(require('./modules/argo-convert').default(argo))
+argo.use(require('./modules/argo-yahoocurrency').default(argo))
 
 // Simple dispatch examples
 argo.dispatch('settings.culture.default').then(console.log)
-argo.dispatch('convert.weight', [10, 'kg', 'g']).then(console.log)
-argo.dispatch('convert.weight', [10, 'g', 'kg']).then(console.log)
+argo.dispatch('convert.weight', [10, 'kg', 'g']).then((res) => {console.log('g->kg', res)})
+argo.dispatch('convert.weight', [10, 'g', 'kg']).then((res) => {console.log('kg->g', res)})
+argo.dispatch('convert.currency', [100, 'aud', 'usd']).then((res) => {console.log('usd->aud', res)})
