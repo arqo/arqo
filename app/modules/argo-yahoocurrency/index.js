d00f03513341f2bc16666af272cb21ca9a1afcb6
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2016-07-03 22:09:04 +0930

    Fix up and finish off yahoo currency plugin

--- app/modules/argo-yahoocurrency/index.js
+++ app/modules/argo-yahoocurrency/index.js
@@ -1,4 +1,4 @@
-import request from 'request'
+import get from 'lodash/get'
 import { only } from '../argo-action-filters'
 
 export default function convertCurrency(app) {
@@ -16,74 +16,87 @@ export default function convertCurrency(app) {
 
     if(settings === null || settings.enabled) {
       const data = await getYahooFinanceCurrencyRates(app, settings)
+      let fromVal, toVal
 
       if (data[from]) {
         fromVal = data[from]['value']
       } else {
-        return null
+        return
       }
 
       if (data[to]) {
         toVal = data[to]['value']
       } else {
-        return null
+        return
       }
 
       return value * (toVal / fromVal)
     }
 
-    return null
+    return
   })
 }
 
 async function getYahooFinanceCurrencyRates(app, settings) {
   const { resolve, dispatch } = app
   const cache = resolve('cache')
-  const duration = settings.duration ? settings.duration : 24 * 60 * 60
+  const duration = settings && settings.duration ? settings.duration : 24 * 60 * 60
 
   let data = await cache.get('yahoo_currency_data')
 
   if(data == null || Date.now() - data.updated > duration) {
-    // Pull new rates from Yahoo
+    const settings = await dispatch('settings')
 
-    let data  = {}
-    let rates = []
-    let [base, currencies] = await Promise.all(
-      dispatch('settings.units.currency'),
-      dispatch('settings.currency')
-    )
+    let rates      = {}
+    let base       = get(settings, 'units.currency', 'USD')
+    let currencies = get(settings, 'currency', [])
 
-    base = base ? base.toUpperCase() : 'USD'
+    base = base.toUpperCase()
 
-    let requestData = currencies.map((c) => {
+    let requestData = Object.keys(currencies).map((c) => {
       return base + c.toUpperCase() + '=X'
     })
 
+    if (requestData.length < 1) {
+      throw new Error('Invalid request data. Have you added currencies to your config?')
+    }
+
     // Example URL format (with USD base): http://download.finance.yahoo.com/d/quotes.csv?s=USDAUD=X,USDUSD=X,USDGBP=X&f=sl1&e=.csv
-    const response = await request('http://download.finance.yahoo.com/d/quotes.csv?s=' + requestData.join(',') + '&f=sl1&e=.csv')
+    const response = await fetch('http://download.finance.yahoo.com/d/quotes.csv?s=' + requestData.join(',') + '&f=sl1&e=.csv')
 
-    if (response) {
-      let lines = response.trim().split("\n")
+    if (!response.ok) {
+      throw new Error('Failed to fetch currency data')
+    }
 
-      lines.forEach((line) => {
-        let value    = parseFloat(line.substring(11, 6))
-        let currency = line.substring(4, 3).toLowerCase()
+    let body  = await response.text()
+    let lines = body.trim().split("\n")
 
-        if (currency === base) {
-          value = 1
-        }
+    if (lines.length < 1) {
+      throw new Error('Currency request data invalid or empty')
+    }
 
-        rates[currency] = { value }
-      })
-
-      data = {
-        updated: Date.now(),
-        rates
+    lines.forEach((line) => {
+      if (line.length < 1) {
+        return
       }
 
-      cache.set('yahoo_currency_data', data)
+      let value    = parseFloat(line.substring(11, 11+6))
+      let currency = line.substring(4, 4+3).toLowerCase()
+
+      if (currency === base) {
+        value = 1
+      }
+
+      rates[currency] = { value }
+    })
+
+    data = {
+      updated: Date.now(),
+      rates
     }
+
+    cache.set('yahoo_currency_data', data)
   }
 
-  return data['rates']
+  return data ? data['rates'] : null
 }
