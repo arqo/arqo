341ce08c3f627eafc5f641b304f670e5f30c0f6c
Patrick Galbraith <patrick.j.galbraith@gmail.com>
2016-07-03 22:08:20 +0930

    Allow settings to be called without qualifying child element

--- app/modules/argo-core/settings.js
+++ app/modules/argo-core/settings.js
@@ -1,12 +1,16 @@
-import { startsWith } from '../argo-action-filters'
+import { only, startsWith } from '../argo-action-filters'
 
 // Returns setting from config
 // Allows easy access to configuration variables
 // Usage: await dispatch(`settings.units.${type}`)
 export default function settings(app, config) {
-  return startsWith('settings.', async (action, next) => {
-    return action.event.split('.').reduce((prev, cur) => {
-      return prev !== null && prev[cur] !== undefined ? prev[cur] : null
-    }, config)
-  })
+  return [
+    only('settings', async (action, next) => config['settings']),
+
+    startsWith('settings.', async (action, next) => {
+      return action.event.split('.').reduce((prev, cur) => {
+        return prev !== null && prev[cur] !== undefined ? prev[cur] : null
+      }, config)
+    })
+  ]
 }
